1b69ea5c695cdeb7683eab48368858aac0a84af8
wangwei <1261385937@qq.com>
2017-12-04 10:11:54 +0800

    UpdateServer进一步完善

--- server/UpdateServer/main.cpp
+++ server/UpdateServer/main.cpp
@@ -4,13 +4,16 @@
 #include <boost/asio/ip/tcp.hpp>
 #include <boost/asio/spawn.hpp>
 #include <boost/asio/write.hpp>
+#include <boost/asio/write_at.hpp>
+#include "boost/asio/read.hpp"
 #include <boost/asio/windows/random_access_handle.hpp>
 
 class session : public std::enable_shared_from_this<session>
 {
 private:
-	boost::asio::windows::random_access_handle file_;
 	boost::asio::ip::tcp::socket socket_;
+	boost::asio::windows::random_access_handle file_;
+
 public:
 	explicit session(boost::asio::io_service& io_service) 
 		:socket_(io_service),
@@ -21,40 +24,58 @@ public:
 
 	void go()
 	{
-		boost::system::error_code ec;
-		file_.assign(CreateFile(L"C:/Users/ww/Desktop/update.exe", GENERIC_WRITE, 0, 0,
-			CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL | FILE_FLAG_OVERLAPPED, 0), ec);
 		auto self = shared_from_this();
 		boost::asio::spawn(socket_.get_io_service(), [this, self](boost::asio::yield_context yield)
 		{
 			try
 			{
-				char data[1024 * 8] = { 0 };
+				char head[16] = { 0 };
+				char data[1008] = { 0 };
 				uint64_t offset = 0;
 				for (;;)
 				{
-					auto n = socket_.async_read_some(boost::asio::buffer(data), yield);
-					file_.async_write_some_at(offset, boost::asio::buffer(data, n), yield);
-					offset += n;
+					boost::asio::async_read(socket_, boost::asio::buffer(head, 16), yield);
+					uint16_t data_legth_net = 0;
+					std::memcpy(&data_legth_net, head + 4, sizeof(uint16_t));
+					auto data_legth = boost::asio::detail::socket_ops::network_to_host_short(data_legth_net);
+					if (head[0] == 1 && head[1] == 0 && head[2] == 0 && head[3] == 2) //content
+					{	
+						boost::asio::async_read(socket_, boost::asio::buffer(data, data_legth), yield);
+						auto ret = boost::asio::async_write_at(file_, offset, boost::asio::buffer(data, data_legth), yield);
+						offset += data_legth;
+					}
+					else if (head[0] == 1 && head[1] == 0 && head[2] == 0 && head[3] == 1) //name
+					{
+						boost::asio::async_read(socket_, boost::asio::buffer(data, data_legth), yield);
+						std::string path_and_name = "C:/Users/ww/Desktop/";
+						path_and_name.append(data);
+						boost::system::error_code ec;
+						file_.assign(CreateFile(path_and_name.c_str(), GENERIC_WRITE, 0, 0,
+							CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL | FILE_FLAG_OVERLAPPED, 0), ec);
+					}
+					else if (head[0] == 1 && head[1] == 0 && head[2] == 0 && head[3] == 0) //can update now
+					{
+						file_.close();
+						socket_.close();
+						STARTUPINFO si;
+						PROCESS_INFORMATION pi;
+						ZeroMemory(&pi, sizeof(pi));
+						ZeroMemory(&si, sizeof(si));
+						char cmdLine[] = "C:/Users/ww/Desktop/update.exe /verysilent";
+						auto ret = CreateProcess(NULL, cmdLine, NULL, NULL, false, 0, NULL, NULL, &si, &pi);
+						if (ret != 0)
+						{
+							CloseHandle(pi.hProcess);
+							CloseHandle(pi.hThread);
+							exit(0);
+						}
+					}
 				}
 			}
 			catch (std::exception& e)
 			{
 				auto err_str = e.what();
-				file_.close();
-				socket_.close();
-				STARTUPINFO si;
-				PROCESS_INFORMATION pi;
-				ZeroMemory(&pi, sizeof(pi));
-				ZeroMemory(&si, sizeof(si));
-				wchar_t cmdLine[] = L"C:/Users/ww/Desktop/update.exe /verysilent";
-				auto ret = CreateProcess(NULL, cmdLine, NULL, NULL, false, 0, NULL, NULL, &si, &pi);
-				if (ret != 0)
-				{
-					CloseHandle(pi.hProcess);
-					CloseHandle(pi.hThread);
-					exit(0);
-				}
+				int zz;
 			}
 		});
 	}
